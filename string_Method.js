3a9dc1eb0e1f0954f087b490c69e5394f860d022
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-01 01:04:57 +0500

    Update string_Method.js

---
+++ string_Method.js
@@ -0,0 +1,11 @@
+// String Methods 
+
+let name = "tehniyat fatima"
+
+console.log(name.length)
+console.log(name.toupperCase())
+console.log(name.tolowerCase())
+console.log(name.slice(0,6))
+console.log(name.slice(6))
+console.log(name.replace("tehniyat fatima", "TEHNIYAT FATIMA"))
+console.log("my name is ",(name.concat()))
