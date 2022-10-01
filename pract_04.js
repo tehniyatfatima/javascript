35a584a40e88e87fd83099cdd02741c8de02a1ba
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-01 23:59:11 +0500

    Update pract_04.js

--- pract_04.js
+++ pract_04.js
@@ -1,5 +1,13 @@
 // practice string method 
 
+console.log("tehniyat fatima")
+
 let sentance = "My name is Tehniyat Fatima "
-const word= "tehniyat"
+const word= "Tehniyat"
 console.log(sentance.includes(word))
+console.log(sentance.startsWith("My"))
+console.log(sentance.endsWith("Tehniyat"))
+
+// Sentance slicing 
+let word2 = "Laptop prize is 45000"
+console.log(word2.slice(16))
