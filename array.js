9db68631c13fde54bb9ea18d182b03a68e014a29
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-02 00:31:04 +0500

    Create array.js

---
+++ array.js
@@ -0,0 +1,12 @@
+// javascript Array
+
+let friends = ["Sara","Areeba Asif","Areesha Aslam","Kiran","Faiza","Shafaq","Muskan"]
+friends[7]="Mahnoor"
+console.log(friends)
+console.log(friends.length)
+
+// quick quiz 
+for (let a in friends){
+        console.log(a)
+};
+        
