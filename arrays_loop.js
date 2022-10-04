b9b08625d989beb082b4e98a91033eca3f33af92
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-04 12:35:39 +0500

    Create arrays_loop.js

---
+++ arrays_loop.js
@@ -0,0 +1,13 @@
+// Arrays with loop
+// array with for loop
+
+let friends = ["Adeena","Amna","Areesha","Sania","Unsa","Dua"]
+
+for (i=0; i<friends.length; i++)[
+        console.log(friends[i])
+];
+
+// for each loop
+friends.forEach(change =>{
+        console.log(change.toUpperCase)
+});
