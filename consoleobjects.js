f490e91493cf247cfbdf70d07bef98cdd59af233
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-06 02:07:21 +0500

    Create consoleobjects.js

---
+++ consoleobjects.js
@@ -0,0 +1,11 @@
+console.log("normal")
+console.error("this is error msg")
+console.clear()
+
+// console table
+obj = {"20B-114-SE":"tehniyat fatima",
+"20B-60-SE":"Adeena Hussuian",
+"20B-028-SE":"Faheem"}
+console.table(obj);
+
+console.warn("this iswarning");
