0afa5fe68d57a0e0aff2ecfecfc083c596afa23a
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-06 02:30:12 +0500

    Update consoleobjects.js

--- consoleobjects.js
+++ consoleobjects.js
@@ -3,9 +3,17 @@ console.error("this is error msg")
 console.clear()
 
 // console table
+console.time()
 obj = {"20B-114-SE":"tehniyat fatima",
 "20B-60-SE":"Adeena Hussuian",
 "20B-028-SE":"Faheem"}
 console.table(obj);
 
 console.warn("this iswarning");
+console.info("this is an important info")
+console.timeEnd()
+
+
+// console time and console time end
+// if you wnat to  messure time, you use 
+
