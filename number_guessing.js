9abb446194b3103891b3c69ecc9cbcefe6518d97
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-07 10:44:43 +0500

    Update number_guessing.js

--- number_guessing.js
+++ number_guessing.js
@@ -1,18 +1,3 @@
-//
-let a = Math.floor((Math.random() * 100) + 1);
-console.log(a)
-let n = prompt("enter number");
-if (n == a ) {
-        console.log("correct gusses , you won the game")
-}
-ifelse (n < a) {
-        console.log("it is lessthen actual number");
-}
-ifelse (n > a) {
-        console.log("number is  greaterthen actual number");
-};
-
-
 console.log("Guess the Number Between 1 and 100");
 let ran = Math.floor((Math.random() * 100) + 1);
 let chances = 0;
