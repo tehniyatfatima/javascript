3a209f2a0c0ac1d30a22ffb3c07b497500a35e36
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-07 11:31:39 +0500

    Create altert_prompt_conform.js

---
+++ altert_prompt_conform.js
@@ -0,0 +1,18 @@
+// alert , altert is used to invok mini window 
+alert("code is working")
+
+// alert and prompt 
+alert("enter your name ")
+//let name = prompt("whats your name")
+console.log(name);
+
+// Conform 
+let name = prompt("whats your name")
+let write = confirm("do want to write your name")
+if (write){
+        console.log(name)
+}
+else{
+        console.log("allow me to write your name")
+};
+
